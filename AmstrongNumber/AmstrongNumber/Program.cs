88fb931539e4a2ba1ec6b0865a0807b89fa099c9
subhadarsini33 <85007766+subhadarsini33@users.noreply.github.com>
2024-01-12 16:49:36 +0530

    Add files via upload

---
+++ AmstrongNumber/AmstrongNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmstrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, temp,b,sum=0;
+
+                Console.WriteLine("Enter Amstrong Numer");
+                int d = int.Parse(Console.ReadLine());
+                temp = d;
+            while(d>0){
+                b = d % 10;
+                sum = sum +( b * b * b);
+                d = d / 10;
+
+            }
+
+            if (temp == sum)
+            {
+                Console.WriteLine("It Is amstrong number");
+            }
+            else  {
+
+                Console.WriteLine("It Is  not a amstrong number");
+            }
+
+            Console.ReadLine();
+       
+      }  
+        
+  }  
+
+  }
+  
