18fb7259c869eb2b20f04a25a65d4c444cb6254f
subhadarsini33 <85007766+subhadarsini33@users.noreply.github.com>
2024-01-12 16:50:55 +0530

    Add files via upload

---
+++ InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            int[] arry = { 3,7,1,10,9,2,5};
+            int n = arry.Length;
+            for (i = 1; i < n;i++ )
+            {
+                int temp = arry[i];
+                 j=i-1;
+                while(j>=0 && arry[j]>temp )
+                {
+                    arry[j + 1] = arry[j];
+                    j = j - 1;
+                }
+                arry[j + 1] = temp;
+            }
+
+            for (i = 1; i < n; ++i) 
+            {
+                Console.Write(arry[i]+" ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
