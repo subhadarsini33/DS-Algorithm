08e45a50f18013091c8c33c899440d5549fa000c
subhadarsini33 <85007766+subhadarsini33@users.noreply.github.com>
2024-01-12 16:47:50 +0530

    Add files via upload

---
+++ PalindromeNumber/PalindromeNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b,sum=0,temp;
+            Console.WriteLine("Enter a number!");
+            int readn = int.Parse(Console.ReadLine());
+            temp = readn;
+            while (readn>0)
+            {
+                a = readn % 10;
+                sum = (sum * 10) + a;
+                readn = readn / 10;
+            }
+
+            if (temp == sum)
+            {
+                Console.WriteLine("It is palendorium number");
+            }
+            else
+            {
+                Console.WriteLine("It is not a palendorium number");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
